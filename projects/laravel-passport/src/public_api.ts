37ad43432eda201ae15be611509f5a194206d0ab
Precious Jahlom Agboado <precious.jahlom@gmail.com>
2018-09-27 05:00:54 +0000

    exposed email password login config

--- projects/laravel-passport/src/public_api.ts
+++ projects/laravel-passport/src/public_api.ts
@@ -3,5 +3,6 @@
  */
 
 export * from './lib/laravel-passport-config';
+export * from './lib/email-password-login-config';
 export * from './lib/laravel-passport.service';
 export * from './lib/laravel-passport.module';
